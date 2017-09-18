9c2a5dd68c3d355a5e4c1c005d25ee963720de63
PatrikBichis <patrik.olsson@x-firm.com>
2017-09-19 00:09:20 +0200

    Added ros pkg

---
+++ ros/LOMAS_ROS_pkg/scripts/watering_node.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# license removed for brevit
+
+def main():
+
+    # Subscribe for teleoperations
+    #rospy.Subscriber("joy", Joy, joyCallback)
+
+    rospy.init_node('watering', anonymous=False)
+
+    rate = rospy.Rate(10)  # 10hz
+
+    while not rospy.is_shutdown():
+
+        rate.sleep()
+        #print("been here!")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+	pass
