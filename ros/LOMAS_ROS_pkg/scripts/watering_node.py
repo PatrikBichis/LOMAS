6f06c0f3718a9c00850d2a4b05b5bc9b832ce1ef
PatrikBichis <patrik.olsson@x-firm.com>
2017-10-03 23:49:45 +0200

    Starting to add logic to watering node

--- ros/LOMAS_ROS_pkg/scripts/watering_node.py
+++ ros/LOMAS_ROS_pkg/scripts/watering_node.py
@@ -2,11 +2,9 @@
 # license removed for brevit
 import rospy
 import roslib
-import serial
 import time
-import argparse
 
-from serial import SerialException
+from pyfirmata import Arduino, util
 from LOMAS_ROS_pkg.msg import watering_status
 from std_msgs.msg import *
 
@@ -19,12 +17,16 @@ status.ErrorNr = 99
 pubWateringStatus = rospy.Publisher('LOMAS_WateringState', watering_status, queue_size=10)
 
 def loadParameters():
+    """
+    Get parameters from ROS param server
+    """
+
     global IsInSimMode
     global Status
     global port
     
     IsInSimMode = rospy.get_param('~sim_port', True)
-    port = rospy.get_param('~port', "/dev/ttyUSB1")
+    port = rospy.get_param('~port', "/dev/ttyACM0")
     status.Interval = rospy.get_param('~watering_interval', 120)
     status.Duration = rospy.get_param('~watering_time', 120)
 
@@ -37,16 +39,24 @@ def loadParameters():
 
 
 def abortCallback(data):
+    """
+    Handling abort comd for watering
+    """
+
     global abort
 
     print 'Abort'
     abort = data.data
 
 
-def intervallCallback(data):
+def intervalCallback(data):
+    """
+    Handling an update of interval parameter
+    """
+
     global status
 
-    print 'Set intervall'
+    print 'Set interval'
     print data.data
 
     status.Interval = data.data
@@ -56,6 +66,10 @@ def intervallCallback(data):
 
 
 def durationCallback(data):
+    """
+    Handling an update of watering duration parameter
+    """
+
     global status
 
     print 'Set duration'
@@ -68,27 +82,51 @@ def durationCallback(data):
 
 
 def cmdCallback(data):
+    """
+    Handling watering cmd
+    """
     global status
-   
+
+    status.Watering = bool(data.data==1)
+
+    board.digital[13].write(data.data)
 
     pubWateringStatus.publish(status)
-    print data
+
+
+def connectToTelemetry(port):
+    """
+    Connect to telemetry controller (Arduino board)
+    """
+
+    global board
+
+    board = Arduino(port)
+    
+    ctrlValue = int(status.Watering==True)
+
+    # Turn of watering
+    board.digital[13].write(ctrlValue)
+    print 'Connected to telemetry control board'
 
 
 def main():
+    global port
 
-    # Subscribe 
+    # Subscribe to topics
     rospy.Subscriber("LOMAS_WateringCmd", std_msgs.msg.UInt8, cmdCallback)
     rospy.Subscriber("LOMAS_WateringAbort", std_msgs.msg.Bool, abortCallback)
-    rospy.Subscriber("LOMAS_WateringSetIntervall", std_msgs.msg.Bool, intervallCallback)
-    rospy.Subscriber("LOMAS_WateringSetDuration", std_msgs.msg.Bool, durationCallback)
+    rospy.Subscriber("LOMAS_WateringSetInterval", std_msgs.msg.UInt8, intervalCallback)
+    rospy.Subscriber("LOMAS_WateringSetDuration", std_msgs.msg.UInt8, durationCallback)
     rospy.loginfo("Starting up watering node")
 
     rospy.init_node('watering', anonymous=False)
 
     loadParameters()
-    #connectToWatering()
+    connectToTelemetry(port)
     pubWateringStatus.publish(status)
+
+    print 'Watering is waiting for command..'
 
     rate = rospy.Rate(10)  # 10hz
 
